9709184d52564893a9c3a5ed934d03a863d8da3f
Arman <life.is2short@outlook.com>
2022-11-13 19:11:21 +0800

    init

---
+++ src/main/java/org/codewitharman/spaceboot/SpaceBootApplication.java
@@ -0,0 +1,13 @@
+package org.codewitharman.spaceboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codewitharman/spaceboot/SpaceBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codewitharman.spaceboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
